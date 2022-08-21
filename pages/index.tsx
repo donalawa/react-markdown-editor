c42158f6ea80d09f62e809d8b7d6bba16257a164
donalawa <awadonalcien12@gmail.com>
2022-08-21 13:25:17 +0100

    UI design complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
 import HeaderMobile from '../components/Header/Header'
 import Sidebar from '../components/Sidebar/Sidbar'
+import Editor from '../components/Editor/Editor'
+
 import styles from '../styles/Home.module.css'
+import Output from '../components/Output/Output'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +14,14 @@ const Home: NextPage = () => {
       </div>
       <div className={styles.main}>
         <HeaderMobile />
+        <div className={styles.contentContainer}>
+          <div className={styles.content}>
+            <Editor />
+          </div>
+          <div className={styles.output}>
+            <Output />
+          </div>
+        </div>
       </div>
     </div>
   )
