5104cddc91ad1a47ed025958e1ee3ff78c7d7d1d
AWA DONACIEN <54797997+donalawa@users.noreply.github.com>
2023-01-03 01:34:37 +0100

    Update Sidbar.tsx

--- components/Sidebar/Sidbar.tsx
+++ components/Sidebar/Sidbar.tsx
@@ -36,12 +36,16 @@ const Sidebar = () => {
         let docs:any = localStorage.getItem('documents');
         let cleanDocs = JSON.parse(docs);
 
-        // console.log('ALL AVAILABLE DOCS');
+        console.log('ALL AVAILABLE DOCS');
+        // console.log(cleanDocs)
         // console.log(JSON.parse(docs));
-        console.log(cleanDocs);
-        if(docs != null) {
+        // console.log(cleanDocs);
+
+        if(cleanDocs != null) {
             setAllFiles(cleanDocs)
         }else {
+            // console.log('DEMO FILES');
+            // console.log(demoContent);
             setAllFiles([demoContent, untitledDocument])
         }
         
@@ -59,8 +63,8 @@ const Sidebar = () => {
             </div>
             <div className={styles.files}>
                 {
-                    allFiles.map((file) => (
-                        <div className={styles.file} onClick={() =>{setActiveContent(file)}}>
+                  allFiles &&  allFiles.map((file, index) => (
+                        <div className={styles.file} key={index} onClick={() =>{setActiveContent(file)}}>
                             <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M13.107 3.393c.167.167.31.393.429.678.119.286.178.548.178.786v10.286c0 .238-.083.44-.25.607a.827.827 0 0 1-.607.25h-12a.827.827 0 0 1-.607-.25.827.827 0 0 1-.25-.607V.857C0 .62.083.417.25.25A.827.827 0 0 1 .857 0h8c.238 0 .5.06.786.179.286.119.512.261.678.428l2.786 2.786ZM9.143 1.214v3.357H12.5c-.06-.172-.125-.294-.196-.366L9.509 1.411c-.072-.072-.194-.137-.366-.197Zm3.428 13.643V5.714H8.857a.827.827 0 0 1-.607-.25.827.827 0 0 1-.25-.607V1.143H1.143v13.714H12.57Z" fill="#FFF"/></svg>
                             <div className={styles.file_info}>
                                 <span className={styles.file_date}>{file.lastUpdatedDate.toString()}</span>
