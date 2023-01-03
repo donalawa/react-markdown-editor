17e9b906225a55e88c8e958aa992e183b93cdffb
AWA DONACIEN <54797997+donalawa@users.noreply.github.com>
2023-01-03 01:34:06 +0100

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -26,8 +26,8 @@ const HeaderMobile = () => {
         <div className={styles.navContainer}>
             <div className={styles.nav_left}>
                 <div className={styles.toggle_icon}>
-                    {!showNavbar && <svg onClick={showNav} width="30" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h30v2H0zM0 8h30v2H0zM0 16h30v2H0z"/></g></svg>}
-                    {showNavbar && <svg onClick={hideNav} width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M2.1.686 23.315 21.9l-1.415 1.415L.686 2.1z"/><path d="M.686 21.9 21.9.685l1.415 1.415L2.1 23.314z"/></g></svg>}
+                    {!showNavbar && <svg onClick={showNav} width="30" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fillRule="evenodd"><path d="M0 0h30v2H0zM0 8h30v2H0zM0 16h30v2H0z"/></g></svg>}
+                    {showNavbar && <svg onClick={hideNav} width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fillRule="evenodd"><path d="M2.1.686 23.315 21.9l-1.415 1.415L.686 2.1z"/><path d="M.686 21.9 21.9.685l1.415 1.415L2.1 23.314z"/></g></svg>}
                 </div>
                 <div className={styles.title}>
                     <h2>MARKDOWN</h2>
